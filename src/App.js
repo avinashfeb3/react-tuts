e87802b9af3452a967ae912fc88f3904c7598108
avinashsng <avinash.webintern@gmail.com>
2024-01-30 23:28:45 +0530

    part-32-array-with-map files uploaded

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    // Example 1
+    const names = ["Alice", "Bob", "Charlie", "Avinash"];
+
+    // Example 2
+    const products=[
+        {
+             id:1,
+            name: "Product 1",
+            price: 10,
+        },
+        {
+             id: 2,
+            name: "Product 2",
+            price: 20,
+        },
+        {
+             id: 3,
+            name: "Product 3",
+            price: 30,
+        },
+        {
+             id: 4,
+            name: "Product 4",
+            price: 40,
+        },
+    ];
+
+  // Example 3 return map function
+    const fruits =["Apple", "Banana", "Orange", "Mango"];
+
+  // Using map to transform the array  of fruits into an array of react elements
+    const fruitItems = fruits.map((fruit, index) =><li key={index}>{fruit}</li>)
+  return (
+      <>
+        <div className="App">
+          <h1>Array With Map</h1>
+        {/* Example 1 */}
+            <ul>
+                {
+                    names.map((name,index)=>(
+                        <li key={index}>{name}</li>
+                    ))}
+            </ul>
+
+            {/* Example 2 */}
+            <ul>
+                {products.map((product)=>(
+                    <li key={product.id}>{product.name} - ${product.price}</li>
+                ))}
+            </ul>
+
+            {/* Example 3 */}
+            <ul>
+                <li>{fruitItems}</li>
+            </ul>
+        </div>
+      </>
+  );
+}
+
+export default App;
