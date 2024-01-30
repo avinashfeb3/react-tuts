27607afb187540342e95818059aa4e08a1381486
avinashsng <avinash.webintern@gmail.com>
2024-01-30 23:36:11 +0530

    part-36-fragment files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import FragmentLayout from "./FragmentLayout";
+
+function App() {
+  return (
+      <div className="App">
+          <h1>Fragment In React</h1>
+          <FragmentLayout/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/FragmentLayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FragmentLayout() {
+  return (
+      <>
+          <div>Fragment</div>
+      </>
+  )
+}
