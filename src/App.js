3b9e9dc5671c0dc71d9383998239dab9bc78f158
avinashsng <avinash.webintern@gmail.com>
2024-01-30 23:03:23 +0530

    part-23-react-componentdidmount-lifecycle files uploaded

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component }from "react";
+import "./App.css";
+
+class App extends Component {
+  // Example 1
+  constructor() {
+    super();
+    this.state = {
+      count:0,
+    };
+    console.log("Constructor called");
+  }
+
+  // Example 2
+  incrementCount = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+  // Example 1
+  componentDidMount(){
+    console.log("Component DidMount!!");
+  }
+  render() {
+    console.log("Render!!");
+    return (
+      <>
+        <div className="App">
+          <h1>React ComponentDidMount</h1>
+
+          {/* Example 2 */}
+          <h2>Count:{this.state.count}</h2>
+          <button onClick={this.incrementCount}>Increment</button>
+        </div>
+      </>
+    );
+  }
+}
+export default App;
