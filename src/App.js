26dcc0586383192268ff774d0dbb54c4db44f2b4
avinashsng <avinash.webintern@gmail.com>
2024-01-30 23:25:12 +0530

    part-30-styles files uploaded

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './App.css';
+
+// Example 2
+import './style.css';
+
+// Example 4
+import Style from './style.module.css';
+
+// Example 5
+import './style.scss';
+function App() {
+  //   Example 3
+    const headline ={
+        color: "#fff",
+        backgroundColor: "blue",
+    };
+  return (
+      <>
+          <div className="App">
+              <h1>Style Methods In React</h1>
+
+              {/* Example 1 Inline CSS */}
+              <h1 style={{color: "#fff", backgroundColor: "green"}}>Inline Method</h1>
+
+              {/* Example 2 Normal CSS */}
+              <h1 className="primary">Inline Method</h1>
+
+              {/* Example 3 CSS In JS */}
+              <h1 style={headline}>CSS In JS</h1>
+
+              {/* Example 4 CSS Module */}
+              <h1 className={Style.title}>CSS Module Method</h1>
+
+              {/* Example 5 Sass & SCSS */}
+              <h1 className="primary-sass">
+                  Sass &
+                   <span>SCSS</span> Method
+              </h1>
+          </div>
+      </>
+  );
+}
+
+export default App;
