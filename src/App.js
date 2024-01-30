4d276a13839fe5f564bb5acc3eab092153452b6d
avinashsng <avinash.webintern@gmail.com>
2024-01-30 22:41:22 +0530

    Part-16-form-handling Files Upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormHandling from './FormHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form Handling</h1>
+      <FormHandling/>
+    </div>
+  );
+}
+
+export default App;
