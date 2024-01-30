af7721d551eab09d4c0b3ad86aa28ab1e455a32e
avinashsng <avinash.webintern@gmail.com>
2024-01-30 23:19:31 +0530

    part-29-useEffect-condition-hook files uploaded

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import UserTotal from "./UserTotal";
+
+function App() {
+
+  const[count, setCount] = useState(0);
+  const[total, setTotal] = useState(100);
+
+  useEffect(() => {
+    // console.log("After Render Total!!");
+  },[total]);
+  useEffect(() => {
+    // console.log("After Render Count!!");
+  },[count]);
+  return (
+    <>
+      <div className="App">
+        <h1>useEffect with Condition</h1>
+        <UserTotal count = {count} total ={total}/>
+
+        {/* Example 2*/}
+        {/* <h2>Count: {count}</h2> */}
+        <button onClick={() => setCount(count+1)}>Update Count</button>
+
+         {/* Example 2*/}
+         {/* <h2>Total: {total}</h2> */}
+        <button onClick={() => setTotal(total+1)}>Update Total</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
