5b0d2fcb7a6f64b1a4383b3a3c6655dec67b926b
avinashsng <avinash.webintern@gmail.com>
2024-01-30 22:44:34 +0530

    Part-17-conditional-renderinf Files Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Profile from "./Profile";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Conditional Rendering Method</h1>
+
+        <Profile />
+      </div>
+    </>
+  );
+}
+export default App;
