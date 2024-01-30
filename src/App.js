d4938e5855e60160788403b077a0e084c0d2874a
avinashsng <avinash.webintern@gmail.com>
2024-01-30 22:46:38 +0530

    Part-18-Form-Validation Files Upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import FormValidation from "./FormValidation";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form Validation</h1>
+      <FormValidation/>
+    </div>
+  );
+}
+
+export default App;
