1955b3cef018731889f20130bf7b3ee33c669f10
avinashsng <avinash.webintern@gmail.com>
2024-02-01 22:44:50 +0530

    part-38-pure-component

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import "./App.css";
+import CounterPureComponent from './CounterPureComponent';
+
+class App extends Component {
+  // Example 1
+  constructor(props){
+    super(props);
+    this.state = {
+      count:0,
+    }
+  }
+
+  // Example 2
+  handleIncrement = () => {
+    this.setState((prevState) =>({
+      // count: prevState.count + 1,
+    }));
+  };
+  render() {
+    console.log("Parent rendered!!");
+    return (
+      <div className='App'>
+        <h1>Pure Component In React.</h1>
+
+        <CounterPureComponent 
+        count={this.state.count} 
+        onIncrement={this.handleIncrement}/>
+      </div>
+    );
+  }
+}
+export default App;
