9e0ce7d1536ca8feab2e2aab146d06328aaa7ede
avinashsng <avinash.webintern@gmail.com>
2024-02-04 13:16:51 +0530

    part-40-memo-hook files uploaded

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useMemo} from "react";
+import "./App.css";
+
+function App() {
+  // Example 1
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [count, setCount] = useState(0);
+
+  // Example 1
+  // const sum = () => {
+  //   console.log("Calculating sum...!");
+  //   return number1 + number2;
+  // };
+
+  // Example 2
+  const sum = useMemo(() => {
+    console.log("Calculating sum...!");
+    return number1 + number2;
+  }, [number1, number2]);
+
+  return (
+    <>
+      <div className="App">
+        <h1>UseMeMo Hook in React</h1>
+
+        {/* Example 1 */}
+        <div>
+          <input
+            type="number"
+            value={number1}
+            onChange={(e) => setNumber1(Number(e.target.value))}
+          />
+          <input
+            type="number"
+            value={number2}
+            onChange={(e) => setNumber2(Number(e.target.value))}
+          />
+          {/* <p>Sum: {sum()}</p> */}
+
+          {/* Example 2 */}
+          <p>Sum: {sum}</p>
+
+          <h2>Count Number: {count}</h2>
+          <button onClick={() => setCount(count + 1)}>Click ++</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
