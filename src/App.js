af79047a1f84a82e19b9e10ec7e81189dc363492
avinashsng <avinash.webintern@gmail.com>
2024-02-05 20:43:09 +0530

    part-42-useref-hook files uploaded

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React,{useRef} from 'react';
+import './App.css';
+function App() {
+  // Example 1
+  const inputRef =  useRef(null);
+
+  // Example 2
+  const handleButtonClick = () => {
+    const inputValue = inputRef.current.value;
+    alert(`Input Value: ${inputValue}`);
+    console.log(`Input Value: ${inputValue}`);
+  } 
+  return (
+    <>
+         <div className="App">
+         <h1>useRef in React</h1>
+
+         {/* Example 1 */}
+         <input ref={inputRef} type="text"/>
+         <button onClick={handleButtonClick}>Get Input Value</button>
+        </div>
+    </>
+  );
+}
+export default App; 
