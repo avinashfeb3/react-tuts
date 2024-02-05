9e03ee5e2e1a695ff4277f874f0debd32e6b3903
avinashsng <avinash.webintern@gmail.com>
2024-02-05 20:34:56 +0530

    part-41-ref-hook files uploaded

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component, createRef } from 'react'
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.myInputRef = createRef();
+  }
+  // Example 1
+  // componentDidMount() {
+  //   this.myInputRef.current.value = "Avinash Singh";
+  //   console.log(this.myInputRef.current.value);
+  // }
+
+  // Example 2
+
+  handleClick = () => {
+      this.myInputRef.current.focus();
+      this.myInputRef.current.style.background = "green";
+      this.myInputRef.current.style.color = "white";
+      console.log(this.myInputRef.current.value);
+
+  };
+  render() {
+    // console.log(this.myInputRef);
+    return (
+      <div className='App'>
+         <h1>Ref Hook in React</h1>
+         {/* Example 1 */}
+          <input type='text' ref={this.myInputRef}/>
+          <button onClick={this.handleClick}>Focus Input</button>
+      </div>
+    )
+  }
+}
+export default App;
