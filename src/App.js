87d9c8741d7e7c600bc3dbd6d5b7a96181bc0c56
DesignerLRS32 <designer@lrsservices.in>
2024-02-06 13:27:22 +0530

    part-44-controlled-component files uploaded

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+
+  // Example 1
+  
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+      setInputValue(event.target.value);
+      console.log(inputValue);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Controlled Component in React</h1>
+
+         {/* Example 1  */}
+         <input type="text"  value={inputValue} onChange={handleChange}/>
+         <p>Current Value: {inputValue}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
