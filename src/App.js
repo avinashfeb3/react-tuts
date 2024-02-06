f4888f9a5ecd750832623ae6d5b2d99ee3b6b1aa
DesignerLRS32 <designer@lrsservices.in>
2024-02-06 15:28:56 +0530

    part-45-uncontrolled-component files uploaded

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React,{useRef} from 'react';
+import './App.css';
+
+function App() {
+  
+  // Example 1
+  let inputRef = useRef(null); 
+  let inputRef2 = useRef(null); 
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("First Input Value:", inputRef.current.value);
+    console.log("Second Input Value:", inputRef2.current.value);
+
+    let input3 = document.getElementById("input3").value;
+    console.log("Input3 Value:", input3);
+  };
+  return (
+    <>
+        <div className="App">
+          <h1>Uncontrolled Component in React</h1>
+
+          <form onSubmit={submitForm}>
+            <input type="text" ref={inputRef}/>
+            <input type="text" ref={inputRef2}/>
+            <input type="text" id="input3"/>
+            <button>Submit</button>
+          </form>
+        </div>
+    </>
+  );
+}
+
+export default App;
