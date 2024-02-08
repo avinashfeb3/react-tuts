53588a57b26d7de538585ceb435e552a5a82c4a0
DesignerLRS32 <designer@lrsservices.in>
2024-02-08 11:57:11 +0530

    part-60-delete-method files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import DeleteMethod from "./DeleteMethod";
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch API Delete Method!</h1>
+
+      {/* Example 1 */}
+      <DeleteMethod />
+    </div>
+  );
+}
+export default App;
