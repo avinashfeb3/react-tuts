7829e47500ddb73c1b804d255d9f4ca6c1a8aeb7
DesignerLRS32 <designer@lrsservices.in>
2024-02-08 11:55:50 +0530

    part-59-put-update-method files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UpdateMethod from "./UpdateMethod";
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch API Put (Update) Method!</h1>
+
+      {/* Example 1 */}
+      <UpdateMethod />
+    </div>
+  );
+}
+export default App;
