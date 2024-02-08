8d7a984f7190378887634334b33e95927050dbf4
DesignerLRS32 <designer@lrsservices.in>
2024-02-08 11:22:47 +0530

    part-56-custom-hook files uploaded

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import CounterComponent from "./CounterComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom Hook</h1>
+
+      {/* Example 1 */}
+      <CounterComponent />
+    </div>
+  );
+}
+export default App;
