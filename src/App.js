8cca3c9df2ff2595d344c7af714d40d9feeb6c4b
DesignerLRS32 <designer@lrsservices.in>
2024-02-08 11:51:28 +0530

    part-57-fetch-api-get-method files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import GetMethod from "./GetMethod";
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch API Get Method!</h1>
+
+      {/* Example 1 */}
+      <GetMethod />
+    </div>
+  );
+}
+export default App;
