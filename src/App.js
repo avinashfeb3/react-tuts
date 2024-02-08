d9866d09d0e1ca1a076b375d06ebfd9d0e2136bd
DesignerLRS32 <designer@lrsservices.in>
2024-02-08 11:54:14 +0530

    part-58-fetch-api-post-method files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import PostMethod from "./PostMethod";
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch API Post Method!</h1>
+
+      {/* Example 1 */}
+      <PostMethod />
+    </div>
+  );
+}
+export default App;
